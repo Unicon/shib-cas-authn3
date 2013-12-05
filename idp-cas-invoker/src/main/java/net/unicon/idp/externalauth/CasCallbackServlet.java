56b5793f93ed1f8a17144cabca869d4f89b5a12b
chasegawa <chasegawa@unicon.net>
2013-12-05 09:23:13 -0700

    Clean up (tightening) of code base

--- idp-cas-invoker/src/main/java/net/unicon/idp/externalauth/CasCallbackServlet.java
+++ idp-cas-invoker/src/main/java/net/unicon/idp/externalauth/CasCallbackServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jasig.cas.client.util.AbstractCasFilter;
+import org.jasig.cas.client.validation.Assertion;
+
 import java.io.IOException;
 
 /**
@@ -18,20 +22,13 @@ import java.io.IOException;
  * @author Andrew Petro
  */
 public class CasCallbackServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        // same session ID as that in the CASified external resource
-        // because session path set to empty at servlet container layer
-        String sessionId = (request.getSession().getId());
-
-        // retrieve the username from the ServletContext keyed by session identifier,
-        // where the CASified resource put it
-        String authenticatedPrincipalName =
-                (String) getServletContext().getAttribute("net.unicon.idp.casauth." + sessionId);
-
-        // clean up the no-longer-needed shared state
-        getServletContext().removeAttribute("net.unicon.idp.casauth." + sessionId);
+        // username from CAS
+        String authenticatedPrincipalName = Assertion.class
+                .cast(request.getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION))
+                .getPrincipal().getName();
 
         //Pass authenticated principal back to IdP to finish its part of authentication request processing
         request.setAttribute(LoginHandler.PRINCIPAL_NAME_KEY, authenticatedPrincipalName);
